cde6712707048940e00efe94913a4123b23b2c19
xHak2215 <147624393+xHak2215@users.noreply.github.com>
2025-10-02 17:46:15 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 import sys
+import random
+import os
+import threading 
+
 import pygame
 
 pygame.init()
+pygame.mixer.init()
 clock = pygame.time.Clock()
 
 # Окно
-window_size = (700, 700)
+window_size = (1100, 800)
 screen = pygame.display.set_mode(window_size)
-pygame.display.set_caption('FNaE')
+pygame.display.set_caption('FNae')
 
 # Шрифт (текст не отрисовывается по требованию, но шрифт оставлен если понадобится)
 font = pygame.font.Font(None, 24)
@@ -50,36 +55,91 @@ def creat_button(x: int, y: int, h: int, w: int, text: str)-> pygame.Rect:
 
     return button_rect
 
+def sprite(x:int, y:int, h:int, w:int, file_name:str):
+    """отрисовка справйтов и реогирование на нажатие
+
+    Args:
+        x (int): X координата
+        y (int): y координата
+        h (int): ширена
+        w (int): высота
+        file_name (str): имя картинки(путь да нее)
+
+    Returns:
+        _class_: Surface
+    """
+    img = pygame.image.load(file_name).convert_alpha()
+    img_r = img.get_rect(bottomright=(x, y))
+    #img.set_colorkey((255, 255, 255))
+    screen.blit(img, img_r)
+    return img_r
+
+def music(music_file:str, loop=-1):
+    pygame.mixer.music.load(music_file)
+    pygame.mixer.music.play(loops=loop)
+
 menu=True #указывает на то что игрок в меню 
 plauing=False #указвыает на то что игрок играет
 
 def main():
-    global menu,plauing
+    global menu, plauing
     while True:
         clock.tick(60)
-        screen.fill((36, 36, 36))  # фон окна
-
-        if menu:
-            exit_button = creat_button(10, 600, 100, 75, "Exit") # создание кнопки
-            start_button = creat_button(10, 540, 100, 75, "Start")
-
+        dis_w, dis_h = pygame.display.get_surface().get_size()
+                
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
-
-            if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
-                if exit_button.collidepoint(event.pos):
+            
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:# открытие меню по нажатию на esc
+                    menu=True 
+                    plauing=False
+                    
+            if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:# это костыль за за которого теперь нельзя выставлять кнопки на координаты 0.0 0.0
+                clic_event=event.pos
+            else:
+                clic_event=0.0,0.0
+                
+            if menu:
+                screen.fill((36, 36, 36))  # фон окна
+                poshalco = sprite(dis_w,dis_h,200,150, os.path.join(os.getcwd(), "asets", "egorka.png"))
+                
+                exit_button = creat_button(10, 700, 100, 75, "Exit") # создание кнопки
+                start_button = creat_button(10, 628, 100, 75, "Start")
+                if exit_button.collidepoint(clic_event):
                     print("exit")
                     pygame.quit()
                     sys.exit()
-                if start_button.collidepoint(event.pos):
+                    
+                if start_button.collidepoint(clic_event):
                     menu=False #отключаем меню
                     plauing=True #включаем игру
-                    print("start")
-
+                    print("start")   
+                    
+                if poshalco.collidepoint(clic_event):
+                    print("пасхлка")# доделать
+                
+            if plauing:
+                #фон
+                img = pygame.image.load(os.path.join(os.getcwd(), "asets", "offise.jpg")).convert_alpha()
+                img = pygame.transform.scale(img, (dis_w, dis_h))# растягиваю на весь экран
+                screen.blit(img, (0,0))
+                
+                open_camera = sprite(dis_w-300, dis_h-10, 150,200, os.path.join(os.getcwd(), "asets", "open_camera.png"))
+                if open_camera.collidepoint(clic_event):
+                    music(os.path.join(os.getcwd(), "asets", "sount", "blip.mp3"), 0)
+                    print("open_camera")
+                    
+                    
         pygame.display.update()
+        
+        
 
 if __name__ == "__main__":
-    main()
-
+    # запускаю музыку и игру в разных потоках
+    thread = threading.Thread(target=music(os.path.join(os.getcwd(), "asets", "sount", ["menu_embiend_2.mp3", "menu_embiendF.mp3"][random.randint(0,1)] )), daemon=True)  
+    mainf = threading.Thread(main(), daemon=True)
+    mainf.start()
+    thread.start()
